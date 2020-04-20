03a5a632cc7f502cdf36d33f04fe0b63f2692185
Dhosio <danielgithiomi@gmail.com>
2020-04-20 15:56:00 +0300

    "This will add the date method to the app"

--- src/main/java/AppRectangle.java
+++ src/main/java/AppRectangle.java
@@ -1,19 +1,23 @@
 
 
 import java.io.Console;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class AppRectangle{
-    public static void main (String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 //        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(System.in));
         Console myConsole = System.console();
+//        char[] charName = {};
 
             System.out.println("=========Welcome to DISCOVER RECTANGLE!=============");
 
             System.out.print("What is your name? ");
                 String enteredName = myConsole.readLine();
-//                char[] charName = enteredName.toCharArray();
-                System.out.println(enteredName.toCharArray());
+//                charName = enteredName.toCharArray();
+//                System.out.println(enteredName.toCharArray());
 
             System.out.print("Hello " + enteredName + ", enter the dimensions below: ");
 
@@ -36,6 +40,15 @@ public class AppRectangle{
                 System.out.println("----------------");
                 System.out.println("The dimensions are not valid!");
             }
+
+//            To print the date rn
+        String inputDate = "08/27/2001";
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date inputDateConverted = new Date();
+
+        inputDateConverted = df.parse(inputDate);
+        SimpleDateFormat sdf = new SimpleDateFormat("E, MMMM d, yyyy");
+        System.out.println(enteredName + "Your date of birth is: " + sdf.format(inputDateConverted));
 
     }
 }
