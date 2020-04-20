158ae6071999e692b030c2394ded257ed9a1a1fb
Dhosio <danielgithiomi@gmail.com>
2020-04-20 14:46:40 +0300

    "This will add the running Rectangle code"

--- src/main/java/AppRectangle.java
+++ src/main/java/AppRectangle.java
@@ -1,5 +1,41 @@
+
+
+import java.io.Console;
+
+
 public class AppRectangle{
     public static void main (String[] args) throws Exception {
+//        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(System.in));
+        Console myConsole = System.console();
+
+            System.out.println("=========Welcome to DISCOVER RECTANGLE!=============");
+
+            System.out.print("What is your name? ");
+                String enteredName = myConsole.readLine();
+//                char[] charName = enteredName.toCharArray();
+                System.out.println(enteredName.toCharArray());
+
+            System.out.print("Hello " + enteredName + ", enter the dimensions below: ");
+
+            System.out.print("Enter the length: ");
+                int enteredLength = Integer.parseInt(myConsole.readLine());
+            System.out.print("Enter the width: ");
+                int enteredWidth = Integer.parseInt(myConsole.readLine());
+
+            Rectangle newRectangle = new Rectangle(enteredLength, enteredWidth);
+
+            newRectangle.ifSquare();
+
+            if (newRectangle.ifSquare() == true) {
+                System.out.println("----------------");
+                System.out.println("The dimensions are for a square");
+            } else if (newRectangle.ifSquare() == false) {
+                System.out.println("----------------");
+                System.out.println("The dimensions are for a rectangle");
+            } else {
+                System.out.println("----------------");
+                System.out.println("The dimensions are not valid!");
+            }
 
     }
 }
